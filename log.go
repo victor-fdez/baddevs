e584a8b22e3ccf7411ec18d6e5b578200380aca1
Victor Fernandez <victor.j.fdez@gmail.com>
2016-10-21 23:27:40 +0000

    fixed issue with categories not stored, redis funcionality working (no garantees), added verbosity

--- log.go
+++ log.go
@@ -5,16 +5,25 @@ import (
 	"github.com/fatih/color"
 )
 
-var good, bad string
+var good, bad, debug string
 
 func init() {
 	good = color.GreenString("<:)")
 	bad = color.RedString("<:( ERROR!")
+	debug = color.YellowString("<:| DEBUG ")
 }
 
 func badDevsInfo(format string, a ...interface{}) (n int, err error) {
 	s := fmt.Sprintf("%v %s", good, format)
 	return fmt.Printf(s, a...)
+}
+
+func badDevsDebug(format string, a ...interface{}) (n int, err error) {
+	if badDevsConfig.verbose {
+		s := fmt.Sprintf("%v %s", debug, format)
+		return fmt.Printf(s, a...)
+	}
+	return 0, nil
 }
 
 func badDevsError(format string, a ...interface{}) (n int, err error) {
